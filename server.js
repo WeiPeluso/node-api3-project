7695ab1aa7f580e76aad2a7da41695551bb30d10
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-07-08 19:18:05 -0400

    all working except post a post from userDB

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
+const userRouter = require("./users/userRouter");
+
+server.use(express.json());
+
+server.use(logger);
+
+server.use("/api/user", userRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `${req.method} to ${req.url} from ${
+      req.hostname
+    } at [${new Date().toISOString()}]`
+  );
+  next();
+}
 
 module.exports = server;
