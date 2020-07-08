c17d2d3f15f3999923c5d7b42121b619271aeabd
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-07-08 19:41:09 -0400

    finished stretch post

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const server = express();
 
 const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
 
 server.use(express.json());
 
 server.use(logger);
 
 server.use("/api/user", userRouter);
+server.use("/api/post", postRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
