72233b545e02bc23724722fcb3dfa6fede7ee6f2
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-07-09 14:25:40 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
 const server = require("./server");
+const PORT = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(PORT, () => {
   console.log("\n* Server Running on http://localhost:4000 *\n");
 });
